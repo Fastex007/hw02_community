12d5cc1056cbe0bec56bd0cbbf50c20a799886e7
Fastex007 <fastex007@yandex.ru>
2020-11-25 18:41:15 +0300

    Final homework. Sprint 3. Bug fixing after review

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -19,4 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='posts')
     group = models.ForeignKey(Group, blank=True, null=True,
-                              on_delete=models.PROTECT)
+                              on_delete=models.SET_NULL, related_name='posts')
+
+    class Meta:
+        ordering = ['-pub_date']
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<slug>', views.group_posts, name="group posts"),
+    path('', views.index, name='index'),
+    path('group/<slug:slug>', views.group_posts, name='group posts'),
 ]
